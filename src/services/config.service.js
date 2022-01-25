7bea956293874a4c57f265f920da4fe634c9102d
Alex Beaty <alex.beaty@iress.com>
2022-01-25 15:09:36 +0000

    change to https

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,7 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'http://quiz-api-smo.us-west-2.elasticbeanstalk.com/';
+    service.apiBase = 'https://quiz-api-smo.us-west-2.elasticbeanstalk.com/';
 
     return service;
 }]);
